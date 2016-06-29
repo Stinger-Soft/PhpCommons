cc57d6d28c4f8d549fa30ff8180b6229c2995f8f
kodde <kodde@kodde-PC.kodde.lan>
2016-06-30 00:39:32 +0200

    improved stability and added relative time difference calculation method
    fixing: prettyPrintMicroTimeInterval now can handle negative numbers
    adding: relative time difference calculation method
    adding: unit tests

--- Tests/StingerSoft/PhpCommons/Formatter/TimeFormatterTest.php
+++ Tests/StingerSoft/PhpCommons/Formatter/TimeFormatterTest.php
@@ -16,11 +16,334 @@ use StingerSoft\PhpCommons\Formatter\TimeFormatter;
 class TimeFormatterTest extends \PHPUnit_Framework_TestCase {
 
 	public function testPrettyPrintMicroTimeInterval() {
+		$this->assertEquals('00:16:40', TimeFormatter::prettyPrintMicroTimeInterval(0, -1000));
+		$this->assertEquals('00:00:10', TimeFormatter::prettyPrintMicroTimeInterval(0, -10));
+		$this->assertEquals('00:00:10', TimeFormatter::prettyPrintMicroTimeInterval(0, -10.2));
+		$this->assertEquals('02:46:40', TimeFormatter::prettyPrintMicroTimeInterval(0, -10000));
+		$this->assertEquals('277:46:40', TimeFormatter::prettyPrintMicroTimeInterval(0, -1000000));
+		
+		$this->assertEquals('00:00:00', TimeFormatter::prettyPrintMicroTimeInterval(0, 0));
+		
 		$this->assertEquals('00:16:40', TimeFormatter::prettyPrintMicroTimeInterval(0, 1000));
 		$this->assertEquals('00:00:10', TimeFormatter::prettyPrintMicroTimeInterval(0, 10));
 		$this->assertEquals('00:00:10', TimeFormatter::prettyPrintMicroTimeInterval(0, 10.2));
-
+		$this->assertEquals('02:46:40', TimeFormatter::prettyPrintMicroTimeInterval(0, 10000));
+		$this->assertEquals('277:46:40', TimeFormatter::prettyPrintMicroTimeInterval(0, 1000000));
 	}
-	
 
+	/**
+	 * @expectedException \InvalidArgumentException
+	 * @dataProvider validGetRelativeTimeToDataProvider
+	 */
+	public function testGetRelativeTimeDifferenceWithInvalidToValues($toValue) {
+		TimeFormatter::getRelativeTimeDifference(true, $toValue);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 * @dataProvider invalidGetRelativeTimeFromDataProvider
+	 */
+	public function testGetRelativeTimeDifferenceWithInvalidFromValues($fromValue) {
+		TimeFormatter::getRelativeTimeDifference($fromValue);
+	}
+
+	public function testGetRelativeTimeDifferenceInSeconds() {
+		$from = time() - 1;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 1);
+		
+		$from = time() - 1;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT1S'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT1S'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 1);
+		
+		$from = time() - 59;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 59);
+		
+		$from = time() - 59;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 59);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT59S'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 59);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT59S'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_SECONDS, 59);
+	}
+
+	public function testGetRelativeTimeDifferenceInMinutes() {
+		$from = time() - 60;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 1);
+		
+		$from = time() - 60;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 1);
+		
+		$from = time() - 60 * 59;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 59);
+		
+		$from = time() - 60 * 59;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 59);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT1M'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT1M'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT59M'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 59);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT59M'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MINUTES, 59);
+	}
+
+	public function testGetRelativeTimeDifferenceInHours() {
+		$from = time() - 60 * 60;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 1);
+		
+		$from = time() - 60 * 60;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 1);
+		
+		$from = time() - 60 * 60 * 23;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 23);
+		
+		$from = time() - 60 * 60 * 23;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 23);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT1H'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT1H'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT23H'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 23);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('PT23H'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_HOURS, 23);
+	}
+
+	public function testGetRelativeTimeDifferenceInDays() {
+		$from = time() - 60 * 60 * 24;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 1);
+		
+		$from = time() - 60 * 60 * 24;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 6;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 6);
+		
+		$from = time() - 60 * 60 * 24 * 6;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 6);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P1D'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P1D'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P6D'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 6);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P6D'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_DAYS, 6);
+	}
+
+	public function testGetRelativeTimeDifferenceInWeeks() {
+		$from = time() - 60 * 60 * 24 * 7;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 7;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 29;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 4);
+		
+		$from = time() - 60 * 60 * 24 * 29;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 4);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P7D'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P7D'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P29D'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 4);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P29D'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_WEEKS, 4);
+	}
+
+	public function testGetRelativeTimeDifferenceInMonths() {
+		$from = time() - 60 * 60 * 24 * 30;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 30;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 30 * 11;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 11);
+		
+		$from = time() - 60 * 60 * 24 * 30 * 11;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 11);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P1M'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P1M'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P11M'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 11);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P11M'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_MONTHS, 11);
+	}
+
+	public function testGetRelativeTimeDifferenceInYears() {
+		$from = time() - 60 * 60 * 24 * 365;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 365;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 1);
+		
+		$from = time() - 60 * 60 * 24 * 365 * 36;
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 36);
+		
+		$from = time() - 60 * 60 * 24 * 365 * 36;
+		$to = time();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 36);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P1Y'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P1Y'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 1);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P36Y'));
+		$to = null;
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 36);
+		
+		$from = new \DateTime();
+		$from->sub(new \DateInterval('P36Y'));
+		$to = new \DateTime();
+		$this->validateDiff(TimeFormatter::getRelativeTimeDifference($from, $to), TimeFormatter::UNIT_YEARS, 36);
+	}
+
+	protected function validateDiff($diff, $expectedUnit, $expectedDiff) {
+		$this->assertCount(2, $diff);
+		$this->assertEquals($expectedUnit, $diff[1]);
+		$this->assertEquals($expectedDiff, $diff[0]);
+	}
+
+	public function validGetRelativeTimeToDataProvider() {
+		return array(
+			array(
+				null 
+			),
+			array(
+				time() 
+			),
+			array(
+				new \DateTime() 
+			) 
+		);
+	}
+
+	public function invalidGetRelativeTimeFromDataProvider() {
+		return array(
+			array(
+				true 
+			),
+			array(
+				"test" 
+			),
+			array(
+				new \stdClass() 
+			) 
+		);
+	}
 }
--- src/StingerSoft/PhpCommons/Formatter/TimeFormatter.php
+++ src/StingerSoft/PhpCommons/Formatter/TimeFormatter.php
@@ -16,9 +16,33 @@ namespace StingerSoft\PhpCommons\Formatter;
  */
 abstract class TimeFormatter {
 
+	const UNIT_SECONDS = 1;
+
+	const UNIT_MINUTES = 2;
+
+	const UNIT_HOURS = 3;
+
+	const UNIT_DAYS = 4;
+
+	const UNIT_WEEKS = 5;
+
+	const UNIT_MONTHS = 6;
+
+	const UNIT_YEARS = 7;
+
+	const MINUTE_IN_SECONDS = 60;
+
+	const HOUR_IN_SECONDS = 3600;
+
+	const DAY_IN_SECONDS = 86400;
+
+	const WEEK_IN_SECONDS = 604800;
+
+	const YEAR_IN_SECONDS = 31536000;
+
 	/**
 	 * Pretty prints an interval specfied by two timestamps
-	 * 
+	 *
 	 * @param float $startTime
 	 *        	Starttime as returned by microtime(true)
 	 * @param float $endTime
@@ -27,7 +51,78 @@ abstract class TimeFormatter {
 	 *        	Locale used to format the result
 	 */
 	public static function prettyPrintMicroTimeInterval($startTime, $endTime) {
-		$seconds = $endTime - $startTime;
+		$seconds = abs($endTime - $startTime);
 		return sprintf('%02d:%02d:%02d', ($seconds / 3600), ($seconds / 60 % 60), $seconds % 60);
+	}
+
+	/**
+	 * Get the relative difference between a given start time and end time.
+	 *
+	 * Depending on the amount of time passed between given from and to, the difference between the two may be
+	 * expressed in seconds, hours, days, weeks, months or years.
+	 *
+	 * @param int|\DateTime $from
+	 *        	the start time, either as <code>DateTime</code> object or as integer expressing a unix timestamp,
+	 *        	used for calculating the relative difference to the given <code>to</code> parameter.
+	 * @param int|\DateTime|null $to
+	 *        	the end time, either as <code>DateTime</code> object, a unix timestamp or <code>null</code> used for
+	 *        	calculating the difference to the given <code>from</code> parameter. In case <code>null</code> is
+	 *        	provided, the current timestamp will be used (utilizing <code>time()</code>).
+	 * @return int[2] returns an array containing two values: first, the relative difference
+	 *         between <code>from</code> and </code>to</code>, second a constant for the time unit of the relative
+	 *         difference. The unit is one of the class constants: <code>UNIT_SECONDS</code>, <code>UNIT_MINUTES</code>,
+	 *         <code>UNIT_HOURS</code>, <code>UNIT_DAYS</code>, <code>UNIT_WEEKS</code>, <code>UNIT_MONTHS</code> or
+	 *         <code>UNIT_YEARS</code>
+	 * @throws \InvalidArgumentException in case <code>from</code> is neither an integer, nor
+	 *         a <code>DateTime</code> object.
+	 * @throws \InvalidArgumentException in case <code>to</code> is neither an integer,
+	 *         nor a <code>DateTime</code> object, nor <code>null</code>.
+	 *        
+	 */
+	public static function getRelativeTimeDifference($from, $to = null) {
+		if($from instanceof \DateTime) {
+			$from = $from->getTimestamp();
+		} else if(!is_int($from)) {
+			throw new \InvalidArgumentException('$from must be a \DateTime or an integer!');
+		}
+		
+		if($to == null) {
+			$to = time();
+		} else if($to instanceof \DateTime) {
+			$to = $to->getTimestamp();
+		} else if(!is_int($to)) {
+			throw new \InvalidArgumentException('$to must be a \DateTime, null or an integer!');
+		}
+		
+		$diff = (int)abs($to - $from);
+		
+		$since = $diff;
+		$unit = self::UNIT_SECONDS;
+		if($diff < self::MINUTE_IN_SECONDS) {
+			// leave empty here, because this is the default case
+		} else if($diff < self::HOUR_IN_SECONDS) {
+			$since = max(1, round($diff / self::MINUTE_IN_SECONDS));
+			$unit = self::UNIT_MINUTES;
+		} elseif($diff < self::DAY_IN_SECONDS && $diff >= self::HOUR_IN_SECONDS) {
+			$since = max(1, round($diff / self::HOUR_IN_SECONDS));
+			$unit = self::UNIT_HOURS;
+		} elseif($diff < self::WEEK_IN_SECONDS && $diff >= self::DAY_IN_SECONDS) {
+			$since = max(1, round($diff / self::DAY_IN_SECONDS));
+			$unit = self::UNIT_DAYS;
+		} elseif($diff < 30 * self::DAY_IN_SECONDS && $diff >= self::WEEK_IN_SECONDS) {
+			$since = max(1, round($diff / self::WEEK_IN_SECONDS));
+			$unit = self::UNIT_WEEKS;
+		} elseif($diff < self::YEAR_IN_SECONDS && $diff >= 30 * self::DAY_IN_SECONDS) {
+			$since = max(1, round($diff / (30 * self::DAY_IN_SECONDS)));
+			$unit = self::UNIT_MONTHS;
+		} elseif($diff >= self::YEAR_IN_SECONDS) {
+			$since = max(1, round($diff / self::YEAR_IN_SECONDS));
+			$unit = self::UNIT_YEARS;
+		}
+		
+		return array(
+			$since,
+			$unit 
+		);
 	}
 }
